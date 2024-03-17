7e7bbf47de7727f1c738b575c2382cbd1399fbed
Jeff Lovell <jeff@lovellworks.com>
2024-03-17 00:07:08 -0400

    implement inference

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ features = torch.tensor(features, dtype = torch.float32)
 label = torch.tensor(label, dtype = torch.float32).reshape(-1, 1)
 
 #create the classifier model
-model_3layer = nn.Sequential(
+model_2layer = nn.Sequential(
     #fully connected hidden layer | 8 => 12
     nn.Linear(8, 15),
     #rectified linear unit activation layer (12 neuron)
@@ -44,7 +44,7 @@ loss_function = nn.BCELoss()
 #   Adam (Adaptive Moment Estimation) optimizer
 #   with the default learning rate of 0.001
 #   This is gradient descent with momentum
-optimizer_algorithm = optim.Adam(model_3layer.parameters(), lr=0.00075)
+optimizer_algorithm = optim.Adam(model_2layer.parameters(), lr = 0.00075)
 
 #train the model
 number_of_epochs = 1000
@@ -55,7 +55,7 @@ for epoch in range(number_of_epochs):
     for i in range(0, len(features), batch_size):
         batch_start = dt.datetime.now()
         features_batch = features[i:i + batch_size]
-        label_prediction = model_3layer(features_batch)
+        label_prediction = model_2layer(features_batch)
         label_batch = label[i:i + batch_size]
         loss = loss_function(label_prediction, label_batch)
         optimizer_algorithm.zero_grad()
@@ -63,9 +63,9 @@ for epoch in range(number_of_epochs):
         optimizer_algorithm.step()
         batch_finish = dt.datetime.now()
         batch_runtime = (batch_finish - batch_start).microseconds
-        with open("model_3layer_training.log", "a") as log:
+        with open("model_2layer_training.log", "a") as log:
             log.write(f"Starting epoch {epoch}, batch {i} at {dt.datetime.now()}\n\tFinished batch {i} of {batch_size * number_of_epochs} in {batch_runtime / 1000} milliseconds ")
-    with open("model_3layer_training.log", "a") as log:
+    with open("model_2layer_training.log", "a") as log:
         log.write(f"Starting epoch {epoch} of {number_of_epochs} {dt.datetime.now()}\n\tFinished epoch {epoch}, latest loss {loss}")
     print(f"Current Time: {dt.datetime.now()} | Finished epoch {epoch}, latest loss {loss}")
 
@@ -74,4 +74,14 @@ run_runtime = (run_finish - run_start).total_seconds()
 
 print(f"Finished training {number_of_epochs} epochs in {run_runtime} seconds | latest loss {loss}\nCheck logs for more info.\nGoodbye.")
 
+#evaluate the model's accuracy
+with torch.no_grad():
+    output_validation = model_2layer(features)
 
+accuracy = (output_validation.round() == label).float().mean()
+print(f"Accuracy {accuracy}")
+
+#make class predictions with the model
+predictions = (model_2layer(features) > 0.5).int()
+for i in range(len(features)):
+    print('%s => %d (expected %d)' % (features[i].tolist(), predictions[i], label[i]))
